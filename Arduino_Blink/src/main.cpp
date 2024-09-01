cda33cc80909f17501685241b5c2bc55336a8240
NathanThang <thangnhathuy@yahoo.com>
2024-09-01 20:27:47 +0700

    init commit

--- Arduino_Blink/src/main.cpp
+++ Arduino_Blink/src/main.cpp
@@ -1,18 +1,18 @@
 #include <Arduino.h>
-
-// put function declarations here:
-int myFunction(int, int);
+#define LED_RED 13
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(13, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(LED_RED, HIGH);
+  delay(2000);
+  digitalWrite(LED_RED, LOW);
+  delay(2000);
+
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+
